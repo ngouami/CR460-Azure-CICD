e2cebfad0f4a3955a215ccf7fe2b3ef25dd03769
ismaelng <ismael.ngouama@etud.polymtl.ca>
2025-10-15 15:41:20 +0000

    fix: remove duplicate outputs from main.tf

--- main.tf
+++ main.tf
@@ -18,5 +18,3 @@ resource "azurerm_subnet" "subnet" {
   address_prefixes     = ["10.10.1.0/24"]
 }
 
-output "vnet_name"   { value = azurerm_virtual_network.vnet.name }
-output "subnet_name" { value = azurerm_subnet.subnet.name }
